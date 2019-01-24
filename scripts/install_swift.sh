848b4cc06f3ef30223a089581c2612dd04c3d3d6
Christoph Pageler <cp@thepeaklab.com>
2019-01-24 15:23:17 +0100

    Add missing var

--- scripts/install_swift.sh
+++ scripts/install_swift.sh
@@ -3,6 +3,8 @@
 # install dependencies
 sudo apt-get install clang libicu-dev -y
 
+pwd=`pwd`
+
 # download and extract swift
 wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
 mkdir $pwd/swift
