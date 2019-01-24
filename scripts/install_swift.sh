ddd04989c69113b77d39a9b76634efe0cc4ac919
Christoph Pageler <cp@thepeaklab.com>
2019-01-24 14:59:05 +0100

    Add pwd output

--- scripts/install_swift.sh
+++ scripts/install_swift.sh
@@ -3,6 +3,8 @@
 # install dependencies
 sudo apt-get install clang libicu-dev -y
 
+pwd
+
 # download and extract swift
 wget https://swift.org/builds/swift-4.1.2-release/ubuntu1404/swift-4.1.2-RELEASE/swift-4.1.2-RELEASE-ubuntu14.04.tar.gz
 mkdir ~/swift
@@ -11,6 +13,7 @@ tar -xvzf swift-4.1.2-RELEASE-ubuntu14.04.tar.gz -C ~/swift
 # make swift available
 export PATH=~/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin:"${PATH}"
 
+pwd
 echo $PATH
 ls -l ~/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin
 
