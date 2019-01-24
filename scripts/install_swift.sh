b031c556d095f6d9e69ca3b1ef713f72b4b26095
Christoph Pageler <cp@thepeaklab.com>
2019-01-24 12:16:11 +0100

    Add Swift install for Linux

---
+++ scripts/install_swift.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# install dependencies
+apt-get install clang libicu-dev -y
+
+# download and extract swift
+wget https://swift.org/builds/swift-4.1.2-release/ubuntu1404/swift-4.1.2-RELEASE/swift-4.1.2-RELEASE-ubuntu14.04.tar.gz
+mkdir ~/swift
+tar -xvzf swift-4.1.2-RELEASE-ubuntu14.04.tar.gz -C ~/swift
+
+# make swift available
+export PATH=~/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin:$PATH
+
+# print swift version
+swift –version
