e26d070775cb405f4d56ed1296653f226f68e9e0
Christoph Pageler <cp@thepeaklab.com>
2019-01-24 15:19:24 +0100

    Update to Swift 4.2.1

--- scripts/install_swift.sh
+++ scripts/install_swift.sh
@@ -3,19 +3,10 @@
 # install dependencies
 sudo apt-get install clang libicu-dev -y
 
-pwd=`pwd`
-
 # download and extract swift
-wget https://swift.org/builds/swift-4.1.2-release/ubuntu1404/swift-4.1.2-RELEASE/swift-4.1.2-RELEASE-ubuntu14.04.tar.gz
+wget https://swift.org/builds/swift-4.2.1-release/ubuntu1404/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu14.04.tar.gz
 mkdir $pwd/swift
-tar -xvzf swift-4.1.2-RELEASE-ubuntu14.04.tar.gz -C $pwd/swift
+tar -xvzf swift-4.2.1-RELEASE-ubuntu14.04.tar.gz -C $pwd/swift
 
 # make swift available
-export PATH=$pwd/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin:"${PATH}"
-
-pwd
-echo $PATH
-ls -l $pwd/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin
-
-# print swift version
-swift -–version
+export PATH=$pwd/swift/swift-4.2.1-RELEASE-ubuntu14.04/usr/bin:"${PATH}"
