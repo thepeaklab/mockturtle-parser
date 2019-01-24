f08d669b456839b12655bd9aac071d1ebc84f71f
Christoph Pageler <cp@thepeaklab.com>
2019-01-24 15:15:32 +0100

    fix tar

--- scripts/install_swift.sh
+++ scripts/install_swift.sh
@@ -8,7 +8,7 @@ pwd=`pwd`
 # download and extract swift
 wget https://swift.org/builds/swift-4.1.2-release/ubuntu1404/swift-4.1.2-RELEASE/swift-4.1.2-RELEASE-ubuntu14.04.tar.gz
 mkdir $pwd/swift
-tar -xvzf swift-4.1.2-RELEASE-ubuntu14.04.tar.gz -C ~/swift
+tar -xvzf swift-4.1.2-RELEASE-ubuntu14.04.tar.gz -C $pwd/swift
 
 # make swift available
 export PATH=$pwd/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin:"${PATH}"
