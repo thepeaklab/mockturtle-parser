1badb2e34753b747091c99112df14ba5c993b13e
Christoph Pageler <cp@thepeaklab.com>
2019-01-24 15:02:18 +0100

    absolute path variable

--- scripts/install_swift.sh
+++ scripts/install_swift.sh
@@ -3,19 +3,19 @@
 # install dependencies
 sudo apt-get install clang libicu-dev -y
 
-pwd
+pwd=`pwd`
 
 # download and extract swift
 wget https://swift.org/builds/swift-4.1.2-release/ubuntu1404/swift-4.1.2-RELEASE/swift-4.1.2-RELEASE-ubuntu14.04.tar.gz
-mkdir ~/swift
+mkdir $pwd/swift
 tar -xvzf swift-4.1.2-RELEASE-ubuntu14.04.tar.gz -C ~/swift
 
 # make swift available
-export PATH=~/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin:"${PATH}"
+export PATH=$pwd/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin:"${PATH}"
 
 pwd
 echo $PATH
-ls -l ~/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin
+ls -l $pwd/swift/swift-4.1.2-RELEASE-ubuntu14.04/usr/bin
 
 # print swift version
 swift -–version
